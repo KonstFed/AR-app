7c500a26c8bbcfb5949df8a45d3f30ffcd44e0aa
Konstantin <konstantin_fedorov_03@mail.ru>
2019-01-30 13:44:20 +0800

    Script new

--- Scripts/GridCreator.cs
+++ Scripts/GridCreator.cs
@@ -15,13 +15,50 @@ public class GridCreator : MonoBehaviour
     float y;
     float new_oldx;
     float new_oldy;
-
+    public width;
+    public heigth;
+    public BuildingMap[] buildings;
+    public int[] path;
     int counter = 0;
-    const eps = 0.5
+    const eps = 0.5;
+    public Paths[] pathWay;
     void Start()
     {
         
     }
+    struct Paths{
+        public x;
+        public y;
+    }
+    struct BuildingMap{
+        public name;
+        public xAxis;
+        public yAxis;
+
+    }
+    public void LoadTXT() {
+
+                string[] readedLines = File.ReadAllLines(".txt"); 
+                int lineCounter = -1;
+                string sepLine;
+                GridWidth = readedLines[0].split(" ")[0];
+                GridHeight = readedLines[0].split(" ")[1];
+                int bias = readedLines[0].split(" ")[2]
+                for(int i = 1; i<bias + 1; i++) {
+                    sepLine = readedLines[i];
+                    BuildingMap[i-1].name = sepLine.split(" ")[0];
+                    BuildingMap[i-1].xAxis = sepLine.split(" ")[1];
+                    BuildingMap[i-1].yAxis = sepLine.split(" ")[2];
+                }
+                int cntPair = readedLines[bias+1];
+                int[] mas = readedLines[bias+2].split(" ");
+
+                for (int j = 0; j < cntPair;j= j + 2){
+                    pathWay[j/2].x  = mas[j];
+                    pathWay[j/2].y = mas[j+1];
+                }
+                //LoadCells(); //Вызов события загрузки блоков
+        }
     void Update()
     {
         if (Abs(x - markers[0].position.x - markers[1].position.x )> eps or Abs(y - markers[0].position.y - markers[1].position.y)>eps){
